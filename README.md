1d7d993e093811962f0af658326afc2f27ebd18d
ShangYJQ <421207553@qq.com>
2025-12-08 23:21:21 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,96 @@
+# Neovim Configuration
+
+A minimal and clean Neovim configuration using the built-in `vim.pack` plugin manager (Neovim 0.12+).
+
+## Requirements
+
+- **Neovim** >= 0.10 (for `vim.pack` support)
+- **Git** (for plugin installation)
+- A [Nerd Font](https://www.nerdfonts.com/) (for icons)
+
+## Features
+
+- **Single-file configuration** – everything lives in `init.lua`
+- **Built-in plugin manager** – uses native `vim.pack` API
+- **Catppuccin Mocha** theme with transparent background
+- **Treesitter** syntax highlighting and folding
+- **Lualine** statusline with git and diagnostics info
+- **Auto pairs** via mini.pairs
+
+## Plugins
+
+| Plugin                                                                | Description                   |
+| --------------------------------------------------------------------- | ----------------------------- |
+| [catppuccin/nvim](https://github.com/catppuccin/nvim)                 | Colorscheme                   |
+| [nvim-web-devicons](https://github.com/nvim-tree/nvim-web-devicons)   | File icons                    |
+| [nvim-treesitter](https://github.com/nvim-treesitter/nvim-treesitter) | Syntax highlighting & folding |
+| [lualine.nvim](https://github.com/nvim-lualine/lualine.nvim)          | Statusline                    |
+| [mini.pairs](https://github.com/echasnovski/mini.pairs)               | Auto pairs                    |
+
+## Keymaps
+
+Leader key: `<Space>`
+
+### General
+
+| Key     | Mode          | Action                 |
+| ------- | ------------- | ---------------------- |
+| `<C-q>` | Insert        | Escape to Normal mode  |
+| `<C-q>` | Normal        | Quit without saving    |
+| `<C-s>` | Normal        | Save file              |
+| `<C-z>` | Normal        | Undo                   |
+| `d`     | Normal/Visual | Delete without yanking |
+
+### Window Management
+
+| Key         | Mode   | Action                 |
+| ----------- | ------ | ---------------------- |
+| `<leader>l` | Normal | Split window right     |
+| `<leader>j` | Normal | Split window below     |
+| `<leader>h` | Normal | Split window left      |
+| `<leader>k` | Normal | Split window above     |
+| `<C-h>`     | Normal | Move to left window    |
+| `<C-j>`     | Normal | Move to window below   |
+| `<C-k>`     | Normal | Move to window above   |
+| `<C-l>`     | Normal | Move to right window   |
+| `<C-Up>`    | Normal | Decrease window height |
+| `<C-Down>`  | Normal | Increase window height |
+| `<C-Left>`  | Normal | Decrease window width  |
+| `<C-Right>` | Normal | Increase window width  |
+
+### Terminal
+
+| Key         | Mode     | Action                  |
+| ----------- | -------- | ----------------------- |
+| `<leader>t` | Normal   | Open terminal at bottom |
+| `<Esc>`     | Terminal | Exit terminal mode      |
+
+## Editor Settings
+
+- **Line numbers**: Relative
+- **Indentation**: 4 spaces (expandtab)
+- **Search**: Case-insensitive (smart case enabled)
+- **Folding**: Treesitter-based expressions
+- **Persistent undo**: Enabled (stored in `~/.local/share/nvim/undodir`)
+- **Scroll offset**: 8 lines vertical, 8 columns horizontal
+- **Smooth scrolling**: Enabled
+- **Window border**: Rounded
+
+## Treesitter Languages
+
+Pre-configured parsers:
+`lua`, `python`, `json`, `vim`, `markdown`, `cpp`, `c`, `rust`, `bash`, `javascript`, `typescript`
+
+## Installation
+
+1. Clone or copy `init.lua` to your Neovim config directory:
+
+   ```
+   # Windows
+   %LOCALAPPDATA%\nvim\init.lua
+
+   # Linux/macOS
+   ~/.config/nvim/init.lua
+   ```
+
+2. Start Neovim – plugins will be installed automatically.
---
+++ init.lua
@@ -0,0 +1,195 @@
+local opt = vim.opt
+local tablen = 4
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+opt.number  = true
+
+opt.relativenumber = true
+
+opt.whichwrap = "<,>,[,],h,l"
+opt.wrap = false
+
+opt.tabstop = 4
+opt.softtabstop = 4
+opt.shiftwidth = tablen
+opt.expandtab = true
+opt.autoindent = true
+
+opt.autoindent = true
+
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+opt.scrolloff = 8
+opt.sidescrolloff = 8
+
+opt.cursorline   = true
+
+opt.smoothscroll = true
+
+local undodir    = vim.fn.stdpath("data") .. "/undodir"
+
+if vim.fn.isdirectory(undodir) == 0 then
+    vim.fn.mkdir(undodir, "p")
+end
+
+opt.undodir = undodir
+opt.undofile = true
+
+opt.shadafile = "NONE"
+
+vim.opt.winborder = 'rounded'
+
+opt.ignorecase = true
+opt.smartcase = true
+
+
+vim.opt.foldmethod = 'expr'                      
+vim.opt.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.opt.foldlevel = 99 
+
+-- neovim plugins
+vim.pack.add({
+    { src = "https://github.com/catppuccin/nvim" },
+    { src = "https://github.com/nvim-tree/nvim-web-devicons"},
+})
+
+vim.pack.add({
+  { src = 'https://github.com/nvim-treesitter/nvim-treesitter' }
+}, {
+  load = function(plug_data)
+    vim.api.nvim_create_autocmd("BufReadPre", {
+      once = true,
+      callback = function()
+        vim.opt.runtimepath:append(plug_data.path)
+        ---@diagnostic disable-next-line: missing-fields
+        require('nvim-treesitter.configs').setup({
+          ensure_installed = { 'lua', 'python', 'json', 'vim', 'markdown','cpp' ,'c' ,'rust' ,'bash' ,'javascript' ,'typescript' },
+          highlight = { enable = true },
+          indent = { enable = true },
+        })
+      end,
+    })
+  end
+})
+
+vim.pack.add({
+    { src = "https://github.com/nvim-lualine/lualine.nvim" }
+}, {
+    load = function(plug_data)
+        vim.opt.runtimepath:append(plug_data.path)
+        
+        require('lualine').setup({
+            options = {
+                icons_enabled = true,
+                theme = 'auto',
+                component_separators = { left = '', right = '' },
+                section_separators = { left = '', right = '' },
+                disabled_filetypes = {
+                    statusline = {},
+                    winbar = {},
+                },
+                ignore_focus = {},
+                always_divide_middle = true,
+                always_show_tabline = true,
+                globalstatus = true,
+                refresh = {
+                    statusline = 100,
+                    tabline = 100,
+                    winbar = 100,
+                }
+            },
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff' },
+                lualine_c = {
+                    {
+                        "diagnostics",
+                        symbols = {
+                            error = " ",
+                            warn = " ",
+                            info = " ",
+                            hint = " ",
+                        },
+                    },
+                    { "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+                    "filename",
+                },
+                lualine_x = {
+                    'encoding', 'fileformat', 'progress'
+                },
+                lualine_y = { 'location' },
+                lualine_z = {
+                    function()
+                        return " " .. os.date("%R")
+                    end,
+                }
+            },
+        })
+    end
+})
+
+vim.api.nvim_create_autocmd("VimEnter", {
+  once = true,
+  callback = function()
+    require("catppuccin").setup({
+        flavour = "mocha",
+  	    transparent_background = true,
+        	float = {
+            	transparent = false,
+            	solid = false,
+        	},
+	})
+    vim.cmd("colorscheme catppuccin")
+  end,
+})
+
+vim.pack.add({
+    { src = "https://github.com/echasnovski/mini.pairs" },
+}, {
+    load = function(plug_data)
+        vim.opt.runtimepath:append(plug_data.path)
+        require('mini.pairs').setup({
+            modes = { insert = true, command = true, terminal = false },
+            skip_next = [=[[%w%%%'%[%"%.%`%$]]=],
+            skip_ts = { "string" },
+            skip_unbalanced = true,
+            markdown = true,
+        })
+    end
+})
+
+-- keymaps
+vim.keymap.set("i", "<C-q>", "<Esc>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-q>", ":q!<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-s>", ":w<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-z>", ":undo<CR>", { noremap = true, silent = true })
+
+vim.keymap.set("n", "d", "\"_d", { noremap = true, silent = true })
+vim.keymap.set("v", "d", "\"_d", { noremap = true, silent = true })
+
+vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<cr>", { desc = "close search highlight" })
+
+vim.keymap.set("n", "<leader>l", ":set splitright<CR>:vsplit<CR>")
+vim.keymap.set("n", "<leader>j", ":set splitbelow<CR>:split<CR>")
+vim.keymap.set("n", "<leader>h", ":set nosplitright<CR>:vsplit<CR>:set splitright<CR>")
+vim.keymap.set("n", "<leader>k", ":set nosplitbelow<CR>:split<CR>:set splitbelow<CR>")
+
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "left" })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "down" })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "up" })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "right" })
+
+vim.keymap.set("n", "<C-Up>", ":resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Down>", ":resize +2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Left>", ":vertical resize -2<CR>", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Right>", ":vertical resize +2<CR>", { noremap = true, silent = true })
+
+-- terminal
+vim.keymap.set("n", "<leader>t", function()
+    vim.cmd("botright 10split | terminal") 
+end, { desc = "open terminal" })
+
+vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { desc = "quit terminal" })
